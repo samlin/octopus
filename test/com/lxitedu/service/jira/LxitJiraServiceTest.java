e331a281246ebb0dd6b3ad1d5a23f848387cc1c2
samlin <samlin@7df5b637-c7b4-2643-9bff-991d5fa3ecdc>
2011-04-11 09:54:20 +0000

    完成的班级注册功能加在菜单上面,
    git-svn-id: http://192.168.1.235/svn/repo/octopus@8670 7df5b637-c7b4-2643-9bff-991d5fa3ecdc

--- test/com/lxitedu/service/jira/LxitJiraServiceTest.java
+++ test/com/lxitedu/service/jira/LxitJiraServiceTest.java
@@ -6,53 +6,53 @@ import com.lxitedu.bean.LxitClass;
 
 //this is samlin add 2010-8-9
 public class LxitJiraServiceTest {
-	private LxitJiraService lxitJiraService = new LxitJiraService();
+  private LxitJiraService lxitJiraService = new LxitJiraService();
 
-	@Test
-	public void testCreateDayProblem() {
-		// fail("Not yet implemented");
-	}
+  @Test
+  public void testCreateDayProblem() {
+    // fail("Not yet implemented");
+  }
 
-	@Test
-	public void testDeleteIssue() {
-		// lxitJiraService.deleteIssueFromClassId("1001");
-	}
+  @Test
+  public void testDeleteIssue() {
+    // lxitJiraService.deleteIssueFromClassId("1001");
+  }
 
-	public void testCreateDayLog() {
-		// LxitClass lxitClass = new LxitClass();
-		// lxitClass.setId("1002");
-		// lxitJiraService.createDayLog(lxitClass);
+  public void testCreateDayLog() {
+    // LxitClass lxitClass = new LxitClass();
+    // lxitClass.setId("1002");
+    // lxitJiraService.createDayLog(lxitClass);
 
-	}
+  }
 
-	@Test
-	public void testCreateStudentFromLxitClass() {
-		LxitClass lxitClass = new LxitClass();
-		lxitClass.setId("1005");
-		lxitJiraService.createGroup(lxitClass.getId());
-		lxitJiraService.createStudnetAndGroup(lxitClass);
+  // @Test
+  public void testCreateStudentFromLxitClass() {
+    LxitClass lxitClass = new LxitClass();
+    lxitClass.setId("1102");
+    lxitJiraService.createGroup(lxitClass.getId());
+    lxitJiraService.createStudnetAndGroup(lxitClass);
 
-	}
+  }
 
-	// @Test
-	public void testCreateDayLogPermission() throws Exception {
-		LxitClass lxitClass = new LxitClass();
-		lxitClass.setId("1002");
-		// lxitJiraService.createGroup(lxitClass.getId());
-		// lxitJiraService.createDayLogPermissionsAndScheme(lxitClass.getId());
-	}
+  // @Test
+  public void testCreateDayLogPermission() throws Exception {
+    LxitClass lxitClass = new LxitClass();
+    lxitClass.setId("1002");
+    // lxitJiraService.createGroup(lxitClass.getId());
+    // lxitJiraService.createDayLogPermissionsAndScheme(lxitClass.getId());
+  }
 
-	// @Test
-	public void testCreateDayLogProject() {
-		LxitClass lxitClass = new LxitClass();
-		lxitClass.setId("1005");
-		// lxitJiraService.createGroup(lxitClass.getId());
-		lxitJiraService.createDayLogPermissionsAndScheme(lxitClass.getId());
-		lxitJiraService.createDayLogProject(lxitClass.getId());
+  @Test
+  public void testCreateDayLogProject() {
+    LxitClass lxitClass = new LxitClass();
+    lxitClass.setId("1102");
+    // lxitJiraService.createGroup(lxitClass.getId());
+    lxitJiraService.createDayLogPermissionsAndScheme(lxitClass.getId());
+    lxitJiraService.createDayLogProject(lxitClass.getId());
 
-	}
+  }
 
-	public void testModifyIssue() throws Exception {
-		lxitJiraService.fixIssue("DLTHREE-4131");
-	}
+  public void testModifyIssue() throws Exception {
+    lxitJiraService.fixIssue("DLTHREE-4131");
+  }
 }
